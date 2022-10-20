c631114941444f130b8d33d979f4095cd4fade2b
Hong Anh Pham <phamhonganh12062000@gmail.com>
2022-10-20 10:16:26 +0700

    Done scenario 1

--- Selenium/SeleDay2/SeleDay2/UnitTest1.cs
+++ Selenium/SeleDay2/SeleDay2/UnitTest1.cs
@@ -3,7 +3,9 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 using SeleniumExtras.WaitHelpers;
+using Assert = NUnit.Framework.Assert;
 
+// SCENARIO 1
 //• Launch the Chrome browser
 //• Open website “http://automationpractice.com/index.php”
 //• Print a Message to display that the website is opened successfully
@@ -22,7 +24,8 @@ namespace SeleDay2
         // shared vars
         protected WebDriverWait _wait;
         protected IWebDriver _driver;
-
+        protected string EXPECTED_FORM_TITLE = "CUSTOMER SERVICE - CONTACT US";
+        protected string EXPECTED_PAGE_TITLE = "My Store"; // typo from "Store" to "store"
         [SetUp]
         // prepare chromedriver
         public void SetUp()
@@ -40,25 +43,37 @@ namespace SeleDay2
 
             // Click to “Contact Us” link on top menu to open Contact us page
             var contactUsButtonLocator = By.XPath("//*[text()='Contact us']");
-            Click(contactUsButtonLocator);
+            IWebElement contactUsButton =  ReturnWebElement(contactUsButtonLocator);
+            contactUsButton.Click();
+
             // Verify the title of the form is “CUSTOMER SERVICE - CONTACT US”
-            var customerServiceTitlelocator = By.XPath("//*[text()='Customer service - Contact us");
-            IWebElement customerServiceTitle = _driver.FindElement(customerServiceTitlelocator);
-            _wait.Until(SeleniumExtras.WaitHelpers.
-                ExpectedConditions.ElementIsVisible(customerServiceTitlelocator));
-            string titleText = 
+            var customerServiceTitlelocator = By.XPath("//*[contains(@class, 'page-heading')]");
+            IWebElement customerServiceTitle = ReturnWebElement(customerServiceTitlelocator);
+            Assert.That(customerServiceTitle.Text, Is.EqualTo(EXPECTED_FORM_TITLE));
+            Console.WriteLine("Text Box Title validated successfully");
 
+            // Come back to Home page (Use ‘Back’ command)
+            _driver.Navigate().Back();
+            // Verify the title of page is “My store”
+            string pageTitle = _driver.Title;
+            Assert.That(pageTitle, Is.EqualTo(EXPECTED_PAGE_TITLE));
+            Console.WriteLine("Text Page Title validated successfully");
 
+            // Again go back to Contact us page (This time use ‘Forward’ command)
+            _driver.Navigate().Forward();
 
+            // Close the Browser
+            _driver.Close();
 
         }
 
         // Find and click elems
-        public void Click(By by)
+        public IWebElement ReturnWebElement(By by)
         {
             IWebElement e = _driver.FindElement(by);
-            _wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(by));
-            e.Click();
+            _wait.Until(SeleniumExtras.WaitHelpers.
+                ExpectedConditions.ElementIsVisible(by));
+            return e;
         }
         [TestCleanup]
         public void CleanUp()
