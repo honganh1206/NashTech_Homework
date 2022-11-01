5bea03fdaf59ea9f0dd93f4f6043f4c56782dc00
Hong Anh Pham <phamhonganh12062000@gmail.com>
2022-11-01 22:35:03 +0700

    Updated FW archi

---
+++ Automation_Test/Automation_Test_Frameworks/TestProject1/DriverCore/WebDriverAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.DriverCore // HomePage inherits WebDriverAction
+{
+    // similar to page obj
+    // Click method uses try/catch
+    // Ctrl A and Command A in Mac
+    // TestContext.Write() to notify try/catch
+
+    // Highlight an elem = Add Style in CSS in FindElem method
+    // Recheck AsyncLocal (for SimpleTest)
+
+
+    internal class WebDriverAction
+    {
+    }
+}
---
+++ Automation_Test/Automation_Test_Frameworks/TestProject1/DriverCore/WebDriverCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.DriverCore
+{
+    // CreateLocalDriver and CreateBrowserStack
+    // Check app config for nuget packages
+    internal class WebDriverCreator
+    {
+        // Receive params to open browsers
+    }
+}
