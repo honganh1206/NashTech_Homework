806728a57c6a4d06ddee7af2c632d94e1e5b556d
Hong Anh Pham <phamhonganh12062000@gmail.com>
2022-11-07 11:20:03 +0700

    Updated some practices

--- SQL/SQL_Practice_Nov_4/SQL_Practice.sql
+++ SQL/SQL_Practice_Nov_4/SQL_Practice.sql
@@ -1,5 +1,4 @@
 -- Practice 1
-
 SELECT * FROM employees WHERE last_name = 'Zlotkey'
 -- Option 1
 SELECT * FROM employees  WHERE department_id = 80 AND last_name <> 'Zlotkey'
@@ -15,7 +14,6 @@ WHERE e2.last_name = 'Zlotkey' AND e1.last_name <> 'Zlotkey'
 SELECT * FROM departments
 -- job_id linked to dbo.jobs
 
----
 -- Practice 2
 --- Calculate avg salary
 SELECT employee_id, last_name, salary 
@@ -24,12 +22,22 @@ WHERE salary > (SELECT AVG(salary) FROM employees)
 ORDER BY salary ASC
 
 -- Practice 3
+SELECT  employee_id, last_name FROM employees e
+WHERE department_id IN 
+		(SELECT department_id FROM employees WHERE last_name LIKE '%u%')
+---- Join
+SELECT distinct e.employee_id, e.last_name FROM employees e
+JOIN employees d ON e.department_id = d.department_id
+WHERE e.last_name = '%u%'
 
 -- Practice 4
 --- on: tren dieu kien nao
+---- Sub-query
 SELECT e.last_name, e.department_id, e.job_id 
 FROM employees e JOIN departments d
 ON e.department_id = d.department_id WHERE location_id = 1700
+
+
 
 -- Practice 4 - Change reqs
 SELECT e.last_name, e.department_id, e.job_id 
@@ -49,10 +57,15 @@ SELECT employee_id FROM employees e
 JOIN departments d on e.department_id = d.department_id where location_id = 1700
 
 -- Practice 5
+--- Sub-query
 SELECT last_name, salary FROM employees
 WHERE manager_id in 
 (SELECT employee_id FROM employees WHERE last_name = 'King')
 
+--- Join
+SELECT e1.last_name, e1.salary FROM employees e1
+JOIN employees e2 ON e1.manager_id = e2.employee_id
+WHERE e2.last_name = 'King'
 
 -- Practice 6
 --- Join
@@ -76,8 +89,19 @@ SELECT employee_id, last_name, salary
 FROM employees
 WHERE department_id IN (SELECT department_id
 			FROM employees
-			WHERE last_name like '%u%')
+			WHERE last_name LIKE '%u%' AND last_name NOT LIKE '%u%u%')
 AND salary > (SELECT AVG(salary)FROM employees);
+
+--- Union
+SELECT employee_id, last_name, salary
+FROM employees
+WHERE department_id IN (SELECT department_id
+			FROM employees
+			WHERE last_name LIKE '%u%' AND last_name NOT LIKE '%u%u%')
+UNION
+SELECT employee_id, last_name, salary
+FROM employees
+WHERE salary > (SELECT AVG(salary) FROM employees)
 
 
 -- Practice 8
@@ -85,6 +109,14 @@ SELECT ROUND(MAX(salary),0) "Maximum",
 ROUND(MIN(salary),0) "Minimum",
 ROUND(SUM(salary),0) "Sum",
 ROUND(AVG(salary),0) "Average"
+FROM employees
+
+--- No decimal point version (db cannot save data in float)
+SELECT 
+CAST(MAX(salary) AS INT),
+CAST(MIN(salary) AS INT),
+CAST(SUM(salary) AS INT),
+CAST(AVG(salary) AS INT)
 FROM employees
 
 -- Practice 9
@@ -96,8 +128,24 @@ OR last_name LIKE 'M%'
 OR last_name LIKE 'A%'
 ORDER BY last_name ;
 
+--- Improved version
+SELECT UPPER(LEFT(last_name,1)) + LOWER(RIGHT(last_name,len(last_name)-1)),LEN(last_name)
+FROM employees
+WHERE last_name LIKE 'J%'
+OR last_name LIKE 'M%'
+OR last_name LIKE 'A%'
+ORDER BY last_name ;
+
+--- Best version
+SELECT UPPER(LEFT(last_name,1)) + LOWER(RIGHT(last_name,len(last_name)-1)),LEN(last_name)
+FROM employees
+WHERE UPPER(SUBSTRING(last_name,1,1)) IN ('J','A','M','L')
+ORDER BY last_name
+
+
+
 -- Practice 10
-SELECT employee_id, last_name, salary, salary + (salary*15/100) "New salary" 
+SELECT employee_id, last_name, salary, CAST(salary + (salary*15/100) AS NUMERIC(10,0)) "New salary" 
 FROM employees;
 
 -- Practice 11
@@ -109,12 +157,16 @@ FROM departments
 ORDER BY 1;
 
 -- Practice 12
-SELECT e.first_name, e.last_name, e.employee_id, e.job_id FROM employees e 
+SELECT e.employee_id FROM employees e 
 JOIN employees m ON e.manager_id = m.employee_id
 WHERE e.hire_date > m.hire_date
 UNION
-SELECT e.first_name, e.last_name, e.employee_id, e.job_id FROM employees e 
+SELECT e.employee_id FROM employees e 
 JOIN departments d on e.department_id = d.department_id
 JOIN locations l on d.location_id = l.location_id
 WHERE city = 'Toronto'
 
+-- MORE
+SELECT * from employees
+INSERT INTO employees(
+SELECT first_name INTO employee_1
